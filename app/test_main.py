d5e7ac8123d081bc67089c8e743af9498cb44188
GeraOSF <gerardogomezromero175@gmail.com>
2024-02-03 15:22:03 -0800

    test: use pytest fixture

--- app/test_main.py
+++ app/test_main.py
@@ -1,16 +1,20 @@
-from app.main import app
+import pytest
 from fastapi.testclient import TestClient
+from app.main import app
 
 
-client = TestClient(app)
+@pytest.fixture(scope="module")
+def test_client():
+    client = TestClient(app)
+    yield client
 
-def test_get_users():
-    response = client.get("/users")
+def test_get_users(test_client):
+    response = test_client.get("/users")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
 
-def test_create_user():
-    response = client.post("/users", json={"email": "emailfromtest@test.com"})
+def test_create_user(test_client):
+    response = test_client.post("/users", json={"email": "emailfromtest@test.com"})
     assert response.status_code == 200
     response = response.json()
     assert isinstance(response["id"], int)
